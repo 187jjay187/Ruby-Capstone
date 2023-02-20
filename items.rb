21de19dadfba56433b0c91e5ca355307e2d4dfaa
Maaloum <50062179+maaloum@users.noreply.github.com>
2023-02-20 18:53:28 +0300

    fix linters

--- items.rb
+++ items.rb
@@ -1,17 +1,18 @@
 require 'date'
 class Item
   attr_reader :archive, :publish_date
- def initialize(publish_date)
-  @id = Random.rand(1..1000)
-  @publish_date = publish_date
-  @archive = false
- end
 
- def can_be_archived?
-  (DateTime.now.year - DateTime.parse(@publish_date).year).to_f > 10 ? true :  false
- end
+  def initialize(publish_date)
+    @id = Random.rand(1..1000)
+    @publish_date = publish_date
+    @archive = false
+  end
 
- def move_to_archive
-  can_be_archived? ? @archive = true : @archive = false
- end
+  def can_be_archived?
+    (DateTime.now.year - DateTime.parse(@publish_date).year).to_f > 10
+  end
+
+  def move_to_archive
+    @archive = can_be_archived?
+  end
 end
