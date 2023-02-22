0ce3fce3b53a83295bd4f034e6032d0d6e73217c
Lukman Abdulkarim <109611523+lukman155@users.noreply.github.com>
2023-02-22 11:23:02 +0100

    Add movie Class

---
+++ class/add_movie.rb
@@ -0,0 +1,42 @@
+require_relative 'items'
+require_relative 'movie'
+require_relative 'source'
+
+class CreateMovies
+  def initialize
+    @movies_list = []
+    @sources = []
+  end
+
+  def add_movie
+    puts 'When was the movie published [mm/dd/yyyy]'
+    publish_date = gets.chomp
+    puts "What is the movie source [e.g. 'From a friend', 'Online shop' ]"
+    src = gets.chomp
+    source = Source.new(src)
+    @sources << source
+    puts 'Is your movie silent? [y/n]:'
+    silent = gets.chomp.downcase
+    case silent
+    when 'y'
+      movie = Movie.new(true, publish_date)
+    when 'n'
+      movie = Movie.new(false, publish_date)
+    end
+    @movies_list << movie
+    puts 'You have added a movie to your movies list.'
+  end
+
+  def list_all_movies
+    puts 'No movies added yet' if @movies_list.empty?
+    @movies_list.each_with_index do |movie, index|
+      puts "Movie ID:#{index} - Silent Movie:#{movie.silent}, Publish Date: #{movie.publish_date}"
+    end
+  end
+
+  def list_all_genre
+    puts 'No sources added yet' if @sources.empty?
+    @sources.each_with_index do |source, index|
+      puts "Source ID:#{index} - "
+
+
---
+++ class/movie.rb
@@ -0,0 +1,24 @@
+require_relative 'items'
+
+class Movie < Item
+  attr_accessor :silent
+  attr_reader :id
+
+  def initialize(silent, publish_date)
+    super(publish_date)
+    @publish_date = publish_date
+    @silent = silent
+    items = []
+  end
+
+  def to_str
+    puts "if_silent #{@silent}, publish_date #{@publish_date}"
+  end
+
+  private
+
+  def can_be_archived?
+    return true if super || @silent
+    return false
+  end
+end
