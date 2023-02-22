5363511bc163ea347c9f401d1dac6014dd8d5d35
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-02-22 11:20:08 +0630

    Fix the linters

--- class/music.rb
+++ class/music.rb
@@ -1,22 +1,23 @@
 require_relative 'items'
 
 class MusicAlbum < Item
- attr_accessor :on_spotify, :public_date
- attr_reader :id
- def initialize(on_spotify,public_date)
-  super(public_date)
-  @public_date = public_date
-  @on_spotify = on_spotify
-  @items = []
- end
+  attr_accessor :on_spotify, :public_date
+  attr_reader :id
 
- def to_s
-  puts "On_spotify #{@on_spotify}, public_date #{@public_date}"
- end
+  def initialize(on_spotify, public_date)
+    super(public_date)
+    @public_date = public_date
+    @on_spotify = on_spotify
+    @items = []
+  end
 
- private
+  def to_s
+    puts "On_spotify #{@on_spotify}, public_date #{@public_date}"
+  end
 
- def can_be_archived?
-  return true if super && @on_spotify
- end
+  private
+
+  def can_be_archived?
+    return true if super && @on_spotify
+  end
 end
