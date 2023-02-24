c281098c6899a372ba811bd7be349a9b821517ef
187jjay187 <187jjay187@gmail.com>
2023-02-24 17:10:14 +0200

    Add author_spec.rb file

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -3,8 +3,9 @@ require_relative '../class/author'
 describe Author do
   context 'Test for the Author' do
     it 'Should test the attribute of the author class' do
-      genre = Author.new('jay')
-      expect(Author.name).to eq 'ely'
+      author = Author.new('jay', 'jerome')
+      expect(author.first_name).to eq 'jay'
+      expect(author.last_name).to eq 'jerome'
     end
   end
 end
