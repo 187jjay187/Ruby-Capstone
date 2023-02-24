39a801d875f2b9fe77ddf2e55533632b5003f3c1
187jjay187 <187jjay187@gmail.com>
2023-02-24 11:59:49 +0200

    Add game_methods.rb file

--- class/game_methods.rb
+++ class/game_methods.rb
@@ -3,7 +3,7 @@ require_relative 'author'
 require_relative 'items'
 # require_relative 'data_store'
 
-class Game
+class GamePlay
   attr_accessor :games, :authors
 
   def initialize
@@ -18,7 +18,7 @@ class Game
     else
       @games.each_with_index do |game, index|
         author_names = game.authors.map { |author_id| find_author_by_id(author_id).full_name }.join(", ")
-        puts "#{index + 1} Multiplayer: #{game.multiplayer}, Last played at: #{game.last_played_at}, Authors: #{author_names}"
+        puts "#{index + 1} Multiplayer: #{game.multiplayer}, Last played at: #{game.last_played_at}, Authors: #{author_names}" # rubocop:disable Layout/LineLength
       end
     end
   end
@@ -32,15 +32,15 @@ class Game
   end
 
   def add_game
-    game_user_input
-    author_user_input
-    authors = @first_name && @last_name ? [find_or_create_author(@first_name, @last_name).id] : []
-    game = Game.new(@multiplayer, @last_played_at, authors, @publish_date)
-    authors.each { |author_id| find_author_by_id(author_id).add_item(game) }
-    @games << game unless @games.include?(game)
-    @data_store.save_games(@games)
-    @data_store.save_authors(@authors)
-    puts 'Game successfully created'
+    input = author_user_input
+    author = Author.new(input[0], input[1])
+    @authors << author
+    game_input = game_user_input
+    puts "When did you publish your game?:"
+    publish_date = gets.chomp
+    game = Game.new(game_input[0], game_input[1], publish_date, author)
+    @games << game
+    puts "you added the game successfully!"
   end
 
   private
@@ -49,31 +49,33 @@ class Game
     puts 'Please fill the following information:'
     puts ''
     print 'Enter a name (Multiplayer): '
-    @multiplayer = $stdin.gets.chomp
-    while @multiplayer.empty? || @multiplayer.length < 2
+    multiplayer = $stdin.gets.chomp
+    while multiplayer.empty? || multiplayer.length < 2
       print 'No name entered, enter a valid name: '
-      @multiplayer = $stdin.gets.chomp
+      multiplayer = $stdin.gets.chomp
     end
     print 'When last did you play (Year)?("YYYY"): '
-    @last_played_at = $stdin.gets.chomp
-    while @last_played_at.empty? || @last_played_at.length != 4
+    last_played_at = $stdin.gets.chomp
+    while last_played_at.empty? || last_played_at.length != 4
       print 'No year entered, enter a valid year("YYYY"): '
-      @last_played_at = $stdin.gets.chomp
+      last_played_at = $stdin.gets.chomp
     end
+    [multiplayer, last_played_at]
   end
 
   def author_user_input
     print 'Enter your first name: '
-    @first_name = $stdin.gets.chomp
-    while @first_name.empty? || @first_name.length < 2
+    first_name = $stdin.gets.chomp
+    while first_name.empty? || first_name.length < 2
       print 'No name entered, enter a valid name: '
-      @first_name = $stdin.gets.chomp
+      first_name = $stdin.gets.chomp
     end
     print 'Enter your last name: '
-    @last_name = $stdin.gets.chomp
-    while @last_name.empty? || @last_name.length < 2
+    last_name = $stdin.gets.chomp
+    while last_name.empty? || last_name.length < 2
       print 'No name entered, enter a valid name: '
-      @last_name = $stdin.gets.chomp
+      last_name = $stdin.gets.chomp
     end
+    [first_name, last_name]
   end
 end
